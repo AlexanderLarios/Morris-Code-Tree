81b25a3284bdcc6dde3e6998c2e331df7045a7fe
Alexander Larios <alarios91@gmail.com>
2017-07-25 19:48:36 -0500

    build tree

--- Project Files/Main.cpp
+++ Project Files/Main.cpp
@@ -3,33 +3,52 @@
 #include <fstream>
 #include <string>
 #include "BTNode.h"
-#include "Binary_Tree.h"
 #include "morse.txt"
 using namespace std;
 
+BTNode* root;
+fstream fin;
+void createTree();
 int main()
 {
-    Binary_Tree<char> morseTree;
-    fstream fin;
+    
     fin.open("morse.txt");
     if (!fin.fail()){
         while(fin.good()){
-            string str;
-            char letter;
-            string morse;
-            BTNode* root = morseTree.getRoot();
-            BTNode* cursor = root;
-            getline(fin,str);
-            letter = str[0];
-            morse = str.substr(1,str.length());
-            for(int i=1; i<str.length(); i++){
-                if(str[i] == '_'){
-                    if(cursor->right == NULL){
-                        cursor->right =  new BTNode() ;
-                    }
-                }
+            createTree();
+    }
+    
+	return 0;
+    }
+}
+
+void createTree(){
+    string str;
+    char letter;
+    string morse;
+    BTNode* cursor = root;
+    getline(fin,str);
+    letter = str[0];
+    morse = str.substr(1,str.length());
+    for(int i=1; i<str.length(); i++){
+        if(str[i] == '_'){
+            if(cursor->right == NULL){
+                cursor->right =  new BTNode();
+                cursor = cursor->right;
             }
         }
+        else if(str[i] == '.'){
+            if(cursor->left == NULL){
+                cursor->left =  new BTNode();
+                cursor = cursor->left;
+            }
+        }
+        else{
+            cout << "bad input" << endl;
+        }
     }
-	return 0;
+    cursor->letter = letter;
+    cursor->morse = morse;
+    
+    
 }
